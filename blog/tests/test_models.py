f3ee2548db6cf2d1d7a5b7e1149a09153185f392
ardak <arda.koc@vestel.com.tr>
2023-11-08 09:17:15 +0300

    added comment model tests.

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -7,11 +7,11 @@ from django.contrib.auth import get_user_model
 from blog import models
 
 
-def create_user(email='user@example.com', password='testpass123'):
+def create_user(username='user1', password='testpass123'):
     """
     Create and return a new user.
     """
-    return get_user_model().objects.create_user(email, password)
+    return get_user_model().objects.create_user(username, password)
 
 
 def create_post(**params):
@@ -33,6 +33,18 @@ def create_tag():
     Create and return a new tag.
     """
     return models.Tag.objects.create(value="Test tag")
+
+
+def create_comment(content_object, content='Example comment content.'):
+    """
+    Create and return a new comment.
+    """
+    user = create_user(username='user2', password='testpass123')
+    return models.Comment.objects.create(
+        creator=user,
+        content=content,
+        content_object=content_object
+    )
 
 
 class ModelTests(TestCase):
@@ -66,3 +78,21 @@ class ModelTests(TestCase):
 
         self.assertEqual(str(post), post.title)
         self.assertEqual(post.tags.count(), 1)
+
+    def test_create_post_comment(self):
+        """
+        Test creating a post comment is successful.
+        """
+        post = create_post()
+        comment = create_comment(post)
+
+        self.assertEqual(comment.content_object, post)
+
+    def test_create_author_comment(self):
+        """
+        Test creating an author comment is successful.
+        """
+        user = create_user(username='author', password='testpass123')
+        comment = create_comment(user)
+
+        self.assertEqual(comment.content_object, user)
