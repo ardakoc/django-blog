95df84bb4c8425e986acad1035a8acf04563a388
ardak <arda.koc@vestel.com.tr>
2023-11-09 10:43:08 +0300

    updated author_details function to add clickable link on authors.

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,5 +1,7 @@
-from django.contrib.auth import get_user_model
 from django import template
+from django.contrib.auth import get_user_model
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 
 register = template.Library()
@@ -8,13 +10,25 @@ register = template.Library()
 @register.filter
 def author_details(author):
     """
-    Returns the author's first and last name, or username.
+    Returns the author's first and last name or username with a
+    clickable link refers to their email if exists.
     """
     user_model = get_user_model()
-    
+
     if not isinstance(author, user_model):
         return ''
-    
+
     if author.first_name and author.last_name:
-        return f'{author.first_name} {author.last_name}'
-    return f'{author.username}'
+        name = escape(f'{author.first_name} {author.last_name}')
+    else:
+        name = escape(f'{author.username}')
+
+    if author.email:
+        email = escape(author.email)
+        prefix = f'<a href="mailto:{email}">'
+        suffix = '</a>'
+    else:
+        prefix = ''
+        suffix = ''
+
+    return mark_safe(f'{prefix}{name}{suffix}')
