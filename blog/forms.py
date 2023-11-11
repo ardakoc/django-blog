9bcf6ba99adf2439cb9f213c836955a44974f4fe
ardak <arda.koc@vestel.com.tr>
2023-11-11 14:43:59 +0300

    added crispy forms to the comment form.

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
 
 from blog.models import Comment
@@ -12,4 +13,5 @@ class CommentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(CommentForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
     
