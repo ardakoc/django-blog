286df4379b6fc70fd5fca14f4ff5cdc247f8b300
ardak <arda.koc@vestel.com.tr>
2023-11-13 16:00:10 +0300

    updated django configurations.

--- app/settings.py
+++ app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
 
 from pathlib import Path
 from configurations import Configuration
@@ -83,12 +84,7 @@ class Dev(Configuration):
     # Database
     # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    DATABASES = values.DatabaseURLValue(f'sqlite:///{BASE_DIR}/db.sqlite3')
 
 
     # Password validation
