b047064ab4950663a75b4a3c6628091ee6ca7d6b
ardak <arda.koc@vestel.com.tr>
2023-11-14 15:54:37 +0300

    installed argon2. updated password hashing algorithm.

--- app/settings.py
+++ app/settings.py
@@ -154,6 +154,14 @@ class Dev(Configuration):
         }
     }
 
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+        'django.contrib.auth.hashers.ScryptPasswordHasher',
+    ]
+
 
 class Prod(Dev):
     DEBUG = False
