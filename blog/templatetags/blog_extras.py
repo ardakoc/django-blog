415421a8af1ea4daeb99be2d28c677f9ec94e9be
ardak <arda.koc@vestel.com.tr>
2023-11-17 15:16:03 +0300

    changed authro username with email in the filter method.

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -27,7 +27,7 @@ def author_details(author, current_user=None):
     if author.first_name and author.last_name:
         name = f'{author.first_name} {author.last_name}'
     else:
-        name = f'{author.username}'
+        name = f'{author.email}'
 
     if author.email:
         prefix = format_html(f'<a href="mailto:{author.email}">')
