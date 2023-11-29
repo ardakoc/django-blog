7538b2b6b6b49fc8158659a86208d120987db698
Arda Koç <ardaskoc@gmail.com>
2023-11-29 10:00:45 +0300

    added auth to the api.

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from blog.api.views import PostDetailAPIView, PostListAPIView
 
 urlpatterns = [
-    path("posts/", PostListAPIView.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetailAPIView.as_view(), name="api_post_detail"),
+    path('posts/', PostListAPIView.as_view(), name='api_post_list'),
+    path('posts/<int:pk>', PostDetailAPIView.as_view(), name='api_post_detail'),
+    path('auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
