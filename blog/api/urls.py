9993eab7f5c7dcfe060da2158a3ed6ac8508c0f1
Arda Koç <ardaskoc@gmail.com>
2023-11-30 10:43:20 +0300

    configured token auth url path.

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework.authtoken import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from blog.api.views import PostDetailAPIView, PostListAPIView
@@ -6,7 +7,9 @@ from blog.api.views import PostDetailAPIView, PostListAPIView
 urlpatterns = [
     path('posts/', PostListAPIView.as_view(), name='api_post_list'),
     path('posts/<int:pk>', PostDetailAPIView.as_view(), name='api_post_detail'),
+    # authentication
     path('auth/', include('rest_framework.urls')),
+    path('token-auth/', views.obtain_auth_token),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
