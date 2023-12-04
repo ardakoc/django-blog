7603023e846e2df976d41e7bdf1f6ab91282cd9f
Arda Koç <ardaskoc@gmail.com>
2023-12-04 15:46:41 +0300

    add user serializer, add related fields to the post serializer

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,10 +1,23 @@
+from blog.models import Post, Tag
+from blog_auth.models import User
 from rest_framework import serializers
-
-from blog.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    tags = serializers.SlugRelatedField(
+        slug_field='value', many=True, queryset=Tag.objects.all()
+    )
+    author = serializers.HyperlinkedRelatedField(
+        queryset=User.objects.all(), view_name='api_user_detail', lookup_field='email'
+    )
+
     class Meta:
         model = Post
         fields = '__all__'
         readonly = ['modified_at', 'created_at']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
