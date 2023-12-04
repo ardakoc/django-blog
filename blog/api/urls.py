3ace42add2d4f744cd0fddaf051e0d4d89e264b6
Arda Koç <ardaskoc@gmail.com>
2023-12-04 15:45:08 +0300

    add user detail api view

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,11 +2,12 @@ from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from blog.api.views import PostDetailAPIView, PostListAPIView
+from blog.api.views import PostDetailAPIView, PostListAPIView, UserDetailAPIView
 
 urlpatterns = [
     path('posts/', PostListAPIView.as_view(), name='api_post_list'),
     path('posts/<int:pk>', PostDetailAPIView.as_view(), name='api_post_detail'),
+    path('users/<str:email>', UserDetailAPIView.as_view(), name='api_user_detail'),
     # authentication
     path('auth/', include('rest_framework.urls')),
     path('token-auth/', views.obtain_auth_token),
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,9 +1,9 @@
-from rest_framework.generics import (ListCreateAPIView,
-                                     RetrieveUpdateDestroyAPIView)
-
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
-from blog.api.serializers import PostSerializer
+from blog.api.serializers import PostSerializer, UserSerializer
 from blog.models import Post
+from blog_auth.models import User
+from rest_framework.generics import (ListCreateAPIView, RetrieveAPIView,
+                                     RetrieveUpdateDestroyAPIView)
 
 
 class PostListAPIView(ListCreateAPIView):
@@ -15,3 +15,9 @@ class PostDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+
+
+class UserDetailAPIView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'email'
