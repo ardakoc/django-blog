a0e278e041e29c564ccf90dc4d675f497d68f186
Arda Koç <ardaskoc@gmail.com>
2023-12-05 12:21:01 +0300

    added post detail serializer. ordered serializer definitions.

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -12,20 +12,6 @@ class TagField(serializers.SlugRelatedField):
             self.fail(f'Tag value {data} is invalid.')
 
 
-class PostSerializer(serializers.ModelSerializer):
-    tags = serializers.SlugRelatedField(
-        slug_field='value', many=True, queryset=Tag.objects.all()
-    )
-    author = serializers.HyperlinkedRelatedField(
-        queryset=User.objects.all(), view_name='api_user_detail', lookup_field='email'
-    )
-
-    class Meta:
-        model = Post
-        fields = '__all__'
-        readonly = ['modified_at', 'created_at']
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -40,3 +26,36 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ['id', 'creator', 'content', 'modified_at', 'created_at']
         readonly = ['modified_at', 'created_at']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    tags = serializers.SlugRelatedField(
+        slug_field='value', many=True, queryset=Tag.objects.all()
+    )
+    author = serializers.HyperlinkedRelatedField(
+        queryset=User.objects.all(), view_name='api_user_detail', lookup_field='email'
+    )
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+        readonly = ['modified_at', 'created_at']
+
+
+class PostDetailSerializer(PostSerializer):
+    comments = CommentSerializer(many=True)
+
+    def update(self, instance, validated_data):
+        comments = validated_data.pop('comments')
+
+        instance = super(PostDetailSerializer, self).update(instance, validated_data)
+
+        for comment_data in comments:
+            if comment_data.get('id'):
+                continue
+            comment = Comment(**comment_data)
+            comment.creator = self.context['request'].user
+            comment.content_object = instance
+            comment.save()
+
+        return instance
