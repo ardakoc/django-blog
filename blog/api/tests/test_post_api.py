73f34ad901dfc2a529631e76f042296f7bd9bf72
Arda Koç <ardaskoc@gmail.com>
2023-12-19 18:34:30 +0300

    added a test method for post creation. fixed timezone errors on testing.

--- blog/api/tests/test_post_api.py
+++ blog/api/tests/test_post_api.py
@@ -3,14 +3,18 @@ Tests for post api.
 """
 from datetime import datetime
 
+from backports import zoneinfo
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils import timezone
-from pytz import UTC
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 
 from blog.models import Post
+
+
+def get_timezone():
+    return zoneinfo.ZoneInfo('Europe/Istanbul')
 
 
 class PostApiTestCase(TestCase):
@@ -48,7 +52,7 @@ class PostApiTestCase(TestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
 
     def test_post_list(self):
-        response = self.client.get('/api/v1/posts')
+        response = self.client.get('/api/v1/posts/')
         data = response.json()
         self.assertEqual(len(data), 2)
 
@@ -64,8 +68,8 @@ class PostApiTestCase(TestCase):
             self.assertEqual(
                 post.published_at,
                 datetime.strptime(
-                    post_dict['published_at'], '%Y-%m-%dT%H:%M:%S.%fZ'
-                ).replace(tzinfo=UTC)
+                    post_dict['published_at'], '%Y-%m-%dT%H:%M:%S.%f+03:00'
+                ).replace(tzinfo=get_timezone())
             )
 
     def test_unauthenticated_post_create(self):
@@ -77,8 +81,28 @@ class PostApiTestCase(TestCase):
             'summary': 'Test Summary',
             'content': 'Test Content',
             'author': 'http://testserver/api/v1/users/test@example.com',
-            'published_at': '2021-01-10T09:00:00Z',
+            'published_at': '2023-12-19T18:10:00Z+03:00',
         }
         response = self.client.post('/api/v1/posts/', post_dict)
         self.assertEqual(response.status_code, 401)
         self.assertEqual(Post.objects.all().count(), 2)
+
+    def test_post_create(self):
+        post_dict = {
+            'title': 'Test Post',
+            'slug': 'test-post-3',
+            'summary': 'Test Summary',
+            'content': 'Test Content',
+            'author': 'http://testserver/api/v1/users/test@example.com',
+            'published_at': '2023-12-19T18:10:00Z+03:00',
+        }
+        response = self.client.post('/api/v1/posts/', post_dict)
+        post_id = response.json()['id']
+        post = Post.objects.get(pk=post_id)
+        self.assertEqual(post.title, post_dict['title'])
+        self.assertEqual(post.slug, post_dict['slug'])
+        self.assertEqual(post.summary, post_dict['summary'])
+        self.assertEqual(post.content, post_dict['content'])
+        self.assertEqual(post.author, self.user1)
+        self.assertEqual(post.published_at, datetime(
+            2023, 12, 19, 18, 10, 0, tzinfo=get_timezone()))
