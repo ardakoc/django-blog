8dd08c62b15e887f445bcdc02f22f6432b247a2a
Arda Koç <ardaskoc@gmail.com>
2023-12-20 10:39:33 +0300

    added a test for creating tag with token auth.

--- blog/api/tests/test_tag_api.py
+++ blog/api/tests/test_tag_api.py
@@ -15,7 +15,7 @@ class TagApiTestCase(LiveServerTestCase):
         get_user_model().objects.create_user(
             email='test@example.com', password='password'
         )
-        
+
         self.tag_values = {"tag1", "tag2", "tag3", "tag4"}
         for value in self.tag_values:
             Tag.objects.create(value=value)
@@ -30,6 +30,18 @@ class TagApiTestCase(LiveServerTestCase):
 
     def test_tag_create_basic_auth(self):
         self.client.auth = HTTPBasicAuth('test@example.com', 'password')
+        response = self.client.post(
+            self.live_server_url + '/api/v1/tags/', {'value': 'tag5'}
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Tag.objects.all().count(), 5)
+
+    def test_tag_create_token_auth(self):
+        token_response = self.client.post(
+            self.live_server_url + '/api/v1/token-auth/',
+            {'username': 'test@example.com', 'password': 'password'},
+        )
+        self.client.headers['Authorization'] = 'Token ' + token_response.json()['token']
         response = self.client.post(
             self.live_server_url + '/api/v1/tags/', {'value': 'tag5'}
         )
