84023060496e7bccfbdc663ddab10009c1def8cf
Arda Koç <ardaskoc@gmail.com>
2023-12-28 14:34:12 +0300

    added jwt authentication to the project.

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path, re_path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from blog.api.schema.views import schema_view
 from blog.api.views import PostViewSet, TagViewSet, UserDetailAPIView
@@ -15,6 +16,8 @@ urlpatterns = [
     # authentication
     path('auth/', include('rest_framework.urls')),
     path('token-auth/', views.obtain_auth_token),
+    path('jwt/', TokenObtainPairView.as_view(), name='jwt_obtain_pair'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt_refresh'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
